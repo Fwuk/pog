ff5ae664db999666b0b0a857d67a798ec5e0472e
Fwuk <75062417+Fwuk@users.noreply.github.com>
2021-10-25 04:17:21 -0400

    Update pictshare.js

--- js/pictshare.js
+++ js/pictshare.js
@@ -28,10 +28,3 @@ $(function() {
         }
       }
   })
-function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val($(element).text()).select();
-  document.execCommand("copy");
-  $temp.remove();
-}
