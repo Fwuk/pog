dff98e55787b8dec731b6ea25ebacb34258172fb
Fwuk <75062417+Fwuk@users.noreply.github.com>
2021-10-26 16:34:55 -0400

    Update pictshare.js

--- js/pictshare.js
+++ js/pictshare.js
@@ -12,7 +12,6 @@ $(function() {
             if(o.status=='ok')
                 $("#uploadinfo").append("<div class='alert alert-success' role='alert'><strong>"+file.name+"</strong> uploaded as <a target='_blank' href='/"+o.hash+"'>"+o.hash+"</a><br/>URL: <a target='_blank' href='"+o.url+"'>"+o.url+"</a></div>")
 
-
             else if(o.status=='err')
                 $("#uploadinfo").append("<div class='alert alert-danger' role='alert'><strong>Error uploading "+file.name+"</strong><br/>Reason: "+o.reason+"</div>")
             console.log(o)
