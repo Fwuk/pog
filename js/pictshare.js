45e5ae9e481ce7ef56014d4e7984931cb91548ee
Fwuk <75062417+Fwuk@users.noreply.github.com>
2021-10-26 16:16:15 -0400

    Update pictshare.js

--- js/pictshare.js
+++ js/pictshare.js
@@ -10,10 +10,10 @@ $(function() {
         {
             var o = JSON.parse(response);
             if(o.status=='ok')
+                $("#uploadinfo").append("<div class='alert alert-success' role='alert'><input type="button" value="Copy Url" onclick="Copy();" /div>
                 $("#uploadinfo").append("<div class='alert alert-success' role='alert'><strong>"+file.name+"</strong> uploaded as <a target='_blank' href='/"+o.hash+"'>"+o.hash+"</a><br/>URL: <a target='_blank' href='"+o.url+"'>"+o.url+"</a></div>")
-            let btn = document.createElement("button");
-btn.innerHTML = "Click Me";
-document.body.appendChild(btn);
+
+
             else if(o.status=='err')
                 $("#uploadinfo").append("<div class='alert alert-danger' role='alert'><strong>Error uploading "+file.name+"</strong><br/>Reason: "+o.reason+"</div>")
             console.log(o)
